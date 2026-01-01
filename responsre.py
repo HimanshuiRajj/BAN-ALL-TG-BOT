1ac602c571a8dd132ca5e55ab7a0887590524c51
HimanshuiRajj <himanshukumarme04@gmail.com>
2026-01-01 21:30:09 +0530

    Create responsre.py

---
+++ responsre.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+photo3 = ""https://graph.org/LOGO-01-01-13
+
+
+def sample_response(input_text):
+  user_message = str(input_text).lower()
+
+  if user_message in ("hi", "hello", "sup", "hiii", "hii", "kha hai"):
+    return f"{photo3}\n\
+    hey! NOOB is not here i am HER Assistant wanna play with me"
+
+  if user_message in ("NOOB", "NOOB", "@NO0B07", "MADARA"):
+    return "HE is busy in her schedule. you can tell me i inform her"
+  if user_message in ("bsdk", "madharchod", "bkl", "behanchod", "cgutiya",
+                      "gaandu"):
+    return "NOOB papa ke bina gaali mat bak aane de use"
+
+  if user_message in ("ladki", "pyar", "dhoka"):
+    return "Pyar ek dhoka hai...aur baaki gamdu ki gaand maar yahi sahi moka hai"
+
+  if user_message in ("time", "time?", "waqt"):
+    now = datetime.now()
+    date_time = now.strftime("%d/%m/%y, %H:%M:%S")
+
+    return f"chal ab time dekh {str(date_time)}. haa dekh liya na time ab bhaag"
+  # return "I don't understand"
+
+
+# from replit import db
